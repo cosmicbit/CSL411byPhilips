91721aeea15f7bcc4951cddf7079f2659ac69392
cosmicbit <philipsjose2003@gmail.com>
2024-09-03 06:58:13 +0530

    still making enfa to nfa

--- 5.eNFA to NFA/main.c
+++ 5.eNFA to NFA/main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdbool.h>
 
 #define MAX_STATes 100
 #define MAX_ALPHABET 26
@@ -9,7 +10,7 @@
 int ENFATransitions[MAX_STATes][MAX_ALPHABET][MAX_NEXT_STATes], nFinalState, finalStates[20], nENFATransitions=0;
 int epsilon_trans[20][2];
 int newStates[MAX_STATes][MAX_STATes];
-int NFATransitions[MAX_STATes][MAX_ALPHABET][1];
+int NFATransitions[MAX_STATes][MAX_ALPHABET];
 
 void initializeENFATransitions() {
     memset(ENFATransitions, -1, sizeof(ENFATransitions));  // No transition initially
@@ -69,8 +70,50 @@ void printETransitions(int nEpsilonTrans){
 		printf("%d e %d\n",epsilon_trans[i][0],epsilon_trans[i][1]);
 }
 
+bool isElementInArray(int arr[], int size, int element) {
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == element) {
+            return true;  // Element found
+        }
+    }
+    return false;  // Element not found
+}
+void printArray(int arr[], int size){
+	for(int i = 0; i < size; i++) {
+        printf("%d\t", arr[i]);
+    }
+	printf("\n");
+}
+void copyArray(int source[], int destination[], int size) {
+    for (int i = 0; i < size; i++) {
+        destination[i] = source[i];
+    }
+}
+
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+bool arraysHaveSameElements(int arr1[], int arr2[], int size, int size1) {
+    // Sort both arrays
+	if(size!=size1)
+		return false;
+    qsort(arr1, size, sizeof(int), compare);
+    qsort(arr2, size, sizeof(int), compare);
+    
+    // Compare sorted arrays
+    for (int i = 0; i < size; i++) {
+        if (arr1[i] != arr2[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
 int main(){
-    int s, n, i, j, k, startState, nNewStates;
+    int s, n, i, j, k, l, t, startState, nNewStates;
+	int temp[MAX_STATes];
 	initializeNewStates();
 	initializeENFATransitions();
 	initializeNFATransitions();
@@ -136,24 +179,62 @@ int main(){
 		printf("}\n");
 	}
     int* currentENFATransition;
+	int nextState[MAX_STATes], m;
 	for(i=0; i<s; i++){
 		printf("state=%d\n", states[i]);
-		k=0;
-		while(newStates[states[i]][k] != -1){
-			printf("Closure[%d]=%d\n", k, newStates[states[i]][k]);
-			for(j=0; j<n; j++){
-				printf("input=%c\n", input[j]);
+		for(j=0; j<n; j++){
+			if(input[j]=='e') continue;
+			printf("input=%c\n", input[j]);
+			memset(temp, -1, sizeof(temp));
+			t=0; // index for temp
+			k=0; // index for closure elements
+			while(newStates[states[i]][k] != -1){
+				printf("Closure[%d]=%d\n", k, newStates[states[i]][k]);
 				currentENFATransition = ENFATransitions[ newStates[ states[i] ][ k ] ][ input[j]-97 ];
-				for (int l = 0; l < MAX_NEXT_STATes; l++)
+				printf("Current ENFA Transition = ");
+				printArray(currentENFATransition, 10);
+				for (l = 0; l < MAX_NEXT_STATes; l++){
 					if(currentENFATransition[l] != -1){
-						printf("%d\t",newStates[states[i]][k]);
-						printf("%c\t",input[j]);
-						printf("%d\n", currentENFATransition[l]);
+						if(!isElementInArray(temp, t, currentENFATransition[l])){
+							temp[t++] = currentENFATransition[l];
+						}
+							
+						//printf("%d\t",newStates[states[i]][k]);
+						//printf("%c\t",input[j]);
+						//printf("%d\n", currentENFATransition[l]);
 					}
-					else
-						break;
-			}	
-			k++;
+					else break;
+				}
+				k++;
+			}
+			printf("\nTemp= ");
+			printArray(temp, t);
+			m=0;
+			for(k=0; k<t; k++){
+				l=0;
+				while(newStates[temp[k]][l]!=-1 && !isElementInArray(nextState, m, newStates[temp[k]][l])){
+					nextState[m++]=newStates[temp[k]][l];
+					l++;
+				}
+				
+			}
+			printf("\nNext State= ");
+			printArray(nextState, m);
+			for(k=0; k<nNewStates; k++){
+				t=0;
+				while(newStates[k][t]!=-1){
+					t++;
+				}
+				printf("\nhere k = %d We are comparing ", states[k]);
+				printArray(newStates[states[k]],t);
+				printf("and ");
+				printArray(nextState,l);
+				if(arraysHaveSameElements(newStates[states[k]], nextState, t, l)){
+					NFATransitions[states[i]][input[j]-97]=states[k];
+					printf("%d %c %d\n", states[i], input[j], states[k]);
+				}
+			}
+			
 		}	
 	}
     return 0;
