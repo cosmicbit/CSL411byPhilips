d16a4f3b52f01baba58881659f55d6f3bd79e55f
cosmicbit <philipsjose2003@gmail.com>
2024-09-11 06:34:33 +0530

    Shift Reduce Parsing completed

--- 7. Shift Reduce Parsing/main.c
+++ 7. Shift Reduce Parsing/main.c
@@ -1,105 +1,107 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
  
 char stack[50];
-//stack[0]='$';
 int tos=-1;
 char buffer[50];
 int cursor=0;
-char substring[1000];
+
  
-void printline(char str[50]){
- 
-	printf("%s\n", str);
-	printf("$%s         ",stack);
-	for(int i=cursor; i<strlen(buffer); i++){
+void printline(char str[]){
+	int i;
+	printf("$");
+	for(i=0; i<=tos; i++){
+		printf("%c", stack[i]);
+	}
+	printf("\t\t");
+	for(i=0; i<cursor; i++)
+		printf(" ");
+	for(i=cursor; i<strlen(buffer); i++){
 		printf("%c", buffer[i]);
 	}
-	printf("        ");
+	printf("$\t\t");
+	printf("%s\n", str);
 }
 // Function to get substr in C
-void getString(int pos, int len, int i, char string[])
+char* getString(int pos, int len, char string[])
 {
- 
- 
-    while (i < len) {
-        substring[i] = string[pos + i - 1];
-        i++;
-    }
- 
-    substring[i] = '\0';
- 
-    // Print the result
-    printf("%s",substring);
-    printf("\n");
+	int j=0;
+	char* substring = (char*)malloc(50 * sizeof(char));
+	for(int i=pos; i<len; i++){
+		substring[j++]=string[i];
+	}
+	substring[j]='\0';
+	return substring;
 }
  
 void pop(){
-	char element;
 	if(tos!=-1)
 		tos--;
+	
 }
+
 void push(char element){
-	char temp[50];
+	char* temp;
 	tos++;
 	stack[tos]=element;
 	stack[tos+1]='\0';
 	int len=strlen(stack);
-	for(int i=0; i<len; i++){
-		strcpy(temp,"");
-		printf("\ntemp=%s\n", temp);
-		printf("i=%d\n", i);
-		printf("curlen=%d\n", (len-i));
-		printf("source=%s\n",(stack+i));
-		getString(i, len-i, 0, stack);
-		strcpy(temp, substring);
-		printf("\ntemp=%s\n", temp);
+	for(int i=0; i<=tos; i++){
+		temp=getString(tos-i, tos+1, stack);
 		if(strcmp(temp,"E+E")==0){
-			pop();
-			pop();
 			printline("reduce E->E+E\0");
+			pop();
+			pop();
+			pop();
+			push('E');
 		}
 		else if(strcmp(temp,"E*E")==0){
-			pop();
-			pop();
 			printline("reduce E->E*E\0");
+			pop();
+			pop();
+			pop();
+			push('E');
 		}
 		else if(strcmp(temp,"(E)")==0){
+			printline("reduce E->(E)\0");
 			pop();
 			pop();
 			pop();
 			push('E');
-			printline("reduce E->(E)\0");
+			
 		}
 		else if(strcmp(temp,"i")==0){
+			printline("reduce E->i\0");
 			pop();
 			push('E');
-			printline("reduce E->i\0");
-		}else if(strcmp(temp, "E")==0){
+			
+		}
+		if(strcmp(stack, "E")==0){
 			if(cursor==strlen(buffer)){
 				printline("Accepted\0");
+				exit(0);
 			}
 		}
 	}
  
 }
 void shift(){
-	push(buffer[cursor]);
 	cursor++;
+	push(buffer[cursor-1]);
 }
- 
 void main(){
 	printf("Enter an input string: ");
-	gets(buffer);
-	printf("\n\nStack        input        action\n");
-	printf("$%s         ",stack);
-	for(int i=cursor; i<strlen(buffer); i++){
-		printf("%c", buffer[i]);
+	if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+		// Remove the newline character if present
+		size_t len = strlen(buffer);
+		if (len > 0 && buffer[len - 1] == '\n') {
+		    buffer[len - 1] = '\0';
+		}
 	}
-	printf("        ");
+	printf("\n\nStack\t\tinput\t\taction\n");
 	for(int i=0; i<strlen(buffer); i++){
- 
-		shift();
 		printline("Shift\0");
+		shift();
 	}
 }
