52d401ad5d7d5ce05da29d8a34c08028c9bbf471
cosmicbit <philipsjose2003@gmail.com>
2024-10-08 20:01:26 +0530

    just a small debugging on const propagation

--- 9.Constant Propagation/main.c
+++ 9.Constant Propagation/main.c
@@ -40,7 +40,7 @@ void constant(){
 	int op1,op2,res;
 	char op,res1[5];
 	for(i=0;i<n;i++){
-		if(isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0])){
+		if((isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0])) || arr[i].op2[0]=='0' ){
 			
 			op1=atoi(arr[i].op1);
 			op2=atoi(arr[i].op2);
